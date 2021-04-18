ddc98270ab5b4ccdcd2ee37b6cc69739d5f7712c
Jason Tan <tanjunxiang@guazi.com>
2021-04-19 00:25:17 +0800

    feat:新增注册页面

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,56 +1,71 @@
 <template>
-	<view class="login">
-		<view>
-			<image class="login-logo" src="../../static/logo/logo.png" mode="aspectFit"></image>
-		</view>
-		<view class="login-form">
-				<view class="cu-form-group">
-					<view class="title">企业捷ID</view>
-					<input placeholder="请输入企业捷ID" name="input" v-model="qyj_id"></input>
-				</view>
-				<view class="cu-form-group">
-					<view class="title">密码</view>
-					<input placeholder="请输入密码" name="input" password v-model="password"></input>
-				</view>
-				<button class="bg-gradual-blue login-button" @click="login">登录</button>
-				<view class="login-button">
-					<span class="left">忘记密码</span>|<span class="right">注册账号</span>
-				</view>
-		</view>
-		
-	</view>
+  <view class="login">
+    <view>
+      <image
+        class="login-logo"
+        src="../../static/logo/logo.png"
+        mode="aspectFit"
+      ></image>
+    </view>
+    <view class="login-form">
+      <view class="cu-form-group">
+        <view class="title">企业捷ID</view>
+        <input placeholder="请输入企业捷ID" name="input" v-model="qyj_id" />
+      </view>
+      <view class="cu-form-group">
+        <view class="title">密码</view>
+        <input
+          placeholder="请输入密码"
+          name="input"
+          password
+          v-model="password"
+        />
+      </view>
+      <button class="bg-gradual-blue login-button" @click="login">登录</button>
+      <view class="login-button">
+        <span class="left">忘记密码</span>
+        |
+        <span class="right" @click="toRegister">注册账号</span>
+      </view>
+    </view>
+  </view>
 </template>
 
-<script>	
-	export default{
-		data:()=>({
-			qyj_id:'',
-			password:''
-		}),
-		methods:{
-			login(){
-				console.log(111,this.qyj_id,this.password)
-			}
-		}
-	}
+<script>
+export default {
+  data: () => ({
+    qyj_id: '',
+    password: ''
+  }),
+  methods: {
+    login() {
+      console.log(111, this.qyj_id, this.password)
+    },
+    toRegister() {
+      uni.navigateTo({
+        url: '/pages/register/register'
+      })
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-	.login{
-		padding: 30px;
-		text-align: center;
-		&-logo{
-			margin-top: 20px;
-			width: 40%;
-		}
-		&-button{
-			margin-top: 30px;
-			.left{
-				margin-right: 10px;
-			}
-			.right{
-				margin-left: 10px;
-			}
-		}
-	}
+.login {
+  padding: 30px;
+  text-align: center;
+  &-logo {
+    margin-top: 20px;
+    width: 40%;
+  }
+  &-button {
+    margin-top: 30px;
+    .left {
+      margin-right: 10px;
+    }
+    .right {
+      margin-left: 10px;
+    }
+  }
+}
 </style>
---
+++ src/pages/register/register.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="register">
+    <div class="register-title">
+      欢迎注册企业捷
+      <image
+        class="register-logo"
+        src="../../static/logo/logo.png"
+        mode="aspectFit"
+      ></image>
+    </div>
+    <view class="register-form">
+      <view class="cu-form-group">
+        <view class="title">企业捷ID</view>
+        <input placeholder="请输入企业捷ID" name="input" v-model="qyj_id" />
+      </view>
+      <view class="cu-form-group">
+        <view class="title">设置密码</view>
+        <input
+          placeholder="请输入密码"
+          name="input"
+          password
+          v-model="password"
+        />
+      </view>
+      <view class="cu-form-group">
+        <view class="title">确认密码</view>
+        <input
+          placeholder="请再次输入密码"
+          name="input"
+          password
+          v-model="password_second"
+        />
+      </view>
+      <button class="bg-gradual-blue register-form" @click="submit">
+        注册
+      </button>
+    </view>
+    <tui-tips ref="toast" position="center"></tui-tips>
+  </div>
+</template>
+
+<script>
+import tuiTips from 'thorui-uni/lib/thorui/tui-tips/tui-tips'
+
+export default {
+  data: () => ({
+    qyj_id: '',
+    password: '',
+    password_second: ''
+  }),
+  components: {
+    tuiTips
+  },
+  methods: {
+    validate() {
+      let options = {
+        msg: '两次输入的密码不一致~',
+        duration: 2000
+      }
+      if (!(this.password === this.password_second)) {
+        this.$refs.toast.showTips(options)
+        return false
+      } else return true
+    },
+    submit() {
+      if (!this.validate()) return
+      console.log(11111)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.register {
+  padding: 30px;
+  text-align: center;
+  &-title {
+    font-size: 26px;
+    display: flex;
+  }
+  &-logo {
+    width: 40px;
+    height: 40px;
+  }
+  &-form {
+    margin-top: 30px;
+  }
+}
+</style>
